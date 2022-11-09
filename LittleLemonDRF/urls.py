444494438357accc5fbe4d7d250b531c8167cc0c
Hasin Hayder <hasin@leevio.com>
2022-11-09 14:31:01 +0600

    Progress

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('categories', views.CategoriesView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
-    path('cart', views.CartView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
     path('orders', views.OrderView.as_view()),
 ]
--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Category, MenuItem, Cart, Order, OrderItem
 from .serializers import CategorySerializer, MenuItemSerializer, CartSerializer, OrderSerializer
+from rest_framework.response import Response
 
 class CategoriesView(generics.ListCreateAPIView):
         queryset = Category.objects.all()
@@ -16,11 +17,23 @@ class CartView(generics.ListCreateAPIView):
         serializer_class = CartSerializer
         permission_classes = [IsAuthenticated]
 
+        def get_queryset(self):
+            return Cart.objects.all().filter(user=self.request.user)
+
+        def delete(self, *args, **kwargs):
+            Cart.objects.all().filter(user=self.request.user).delete()
+            return Response("ok")
+
 class OrderView(generics.ListCreateAPIView):
         queryset = Order.objects.all()
         serializer_class = OrderSerializer
         permission_classes = [IsAuthenticated]
 
+        def create(self, request, *args, **kwargs):
+            serializer = OrderSerializer(data=request.data)
+            if(serializer.is_valid()):
+                order = serializer.save()
+                return Response(order.id)
 
         
 
